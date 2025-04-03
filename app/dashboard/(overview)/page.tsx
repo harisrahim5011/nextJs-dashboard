9b95a0a86a7173120442c561589067d58c21f86f
harisrahim5011 <harisrahim5011@gmail.com>
2025-04-04 00:35:43 +0300

    component streaming at invoices done.

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,13 +2,20 @@ import { Card } from "@/app/ui/dashboard/cards";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
-import { fetchRevenue, fetchCardData, fetchLatestInvoices } from "../../lib/data";
-import { RevenueChartSkeleton } from "@/app/ui/skeletons";
+import {
+  fetchRevenue,
+  fetchCardData,
+  fetchLatestInvoices,
+} from "../../lib/data";
+import {
+  RevenueChartSkeleton,
+  LatestInvoicesSkeleton,
+} from "@/app/ui/skeletons";
 import { Suspense } from "react";
 
 export default async function Page() {
   // const revenue = await fetchRevenue();
-  const latestInvoices = await fetchLatestInvoices();
+  // const latestInvoices = await fetchLatestInvoices();
   const cardData = await fetchCardData();
   return (
     <main>
@@ -16,9 +23,21 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Card title="Collected" value={cardData.totalPaidInvoices} type="collected" />
-        <Card title="Pending" value={cardData.totalPendingInvoices} type="pending" />
-        <Card title="Total Invoices" value={cardData.numberOfInvoices} type="invoices" />
+        <Card
+          title="Collected"
+          value={cardData.totalPaidInvoices}
+          type="collected"
+        />
+        <Card
+          title="Pending"
+          value={cardData.totalPendingInvoices}
+          type="pending"
+        />
+        <Card
+          title="Total Invoices"
+          value={cardData.numberOfInvoices}
+          type="invoices"
+        />
         <Card
           title="Total Customers"
           value={cardData.numberOfCustomers}
@@ -30,7 +49,10 @@ export default async function Page() {
         <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <Suspense fallback={<LatestInvoicesSkeleton></LatestInvoicesSkeleton>}>
+          <LatestInvoices></LatestInvoices>
+        </Suspense>
+        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
       </div>
     </main>
   );
